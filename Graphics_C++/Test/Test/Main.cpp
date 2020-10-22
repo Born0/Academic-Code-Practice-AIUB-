cf6eaa78374ba7ee5c9bf3e83c46e232ffb17f5a
Md.Sirajuddin Borno <40140598+Born0@users.noreply.github.com>
2020-10-22 09:35:35 +0600

    .

--- Graphics_C++/Test/Test/Main.cpp
+++ Graphics_C++/Test/Test/Main.cpp
@@ -9,18 +9,51 @@ void myDisplay(void)
 	glPointSize(10.0);
 
 	glBegin(GL_LINES);
+	glVertex2i(320, 0);
+	glVertex2i(320, 480);
+	glEnd();
+
+	glBegin(GL_LINES);
+	glVertex2i(0, 240);
+	glVertex2i(640, 240);
+	glEnd();
+
+	glBegin(GL_LINES);
+	glVertex2i(100, 0);
+	glVertex2i(100, 200);
+	glVertex2i(200, 0);
+	glVertex2i(200, 200);
 	glVertex2i(100, 50);
-	glVertex2i(100, 100);
+	glVertex2i(200, 50);
+	glVertex2i(100, 150);
+	glVertex2i(200, 150);
 	glEnd();
 
-	glBegin(GL_LINES);
-	glVertex2i(150, 50);
-	glVertex2i(150, 100);
+	glBegin(GL_TRIANGLES);
+	glVertex2i(100, 250);
+	glVertex2i(200, 250);
+	glVertex2i(150, 340);
+	glVertex2i(150, 340);
+	glVertex2i(100, 440);
+	glVertex2i(200, 440);
 	glEnd();
 
-	glBegin(GL_LINES);
-	glVertex2i(100, 75);
-	glVertex2i(150, 75);
+	glBegin(GL_TRIANGLES);
+	glVertex2i(350, 50);
+	glVertex2i(550, 50);
+	glVertex2i(450, 150);
+	glEnd();
+
+	glBegin(GL_QUADS);
+	glVertex2i(450, 250);
+	glVertex2i(600, 250);
+	glVertex2i(600, 400);
+	glVertex2i(450, 400);
+
+	glVertex2i(350, 300);
+	glVertex2i(500, 300);
+	glVertex2i(500, 440);
+	glVertex2i(350, 440);
 	glEnd();
 
 	glFlush();
@@ -40,7 +73,7 @@ int main(int argc, char** argv)
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowSize(640, 480);
 	glutInitWindowPosition(100, 150);
-	glutCreateWindow("H");
+	glutCreateWindow("Display");
 	glutDisplayFunc(myDisplay);
 	myInit();
 	glutMainLoop();
